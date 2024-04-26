39e8d83f0cd56da0841d933087ba8211e2912ea8
209410686 <209410686@gms.tku.edu.tw>
2024-04-26 21:45:23 +0800

    w10-p1

---
+++ mid21_86/client_86/src/App_86.jsx
@@ -0,0 +1,60 @@
+import { BrowserRouter, Routes, Route, Router, createBrowserRouter } from 'react-router-dom';
+
+import HomePage_86 from './pages/HomePage_86';
+import HomeLayout_86  from './pages/HomeLayout_86';
+import SupaGetBlog_86 from './pages/demo_86/SupaGetBlog_86';
+import Mid1SupaBlog_86 from './pages/mid1_86/Mid1SupaBlog_86';
+import Mid2NodeBlog_86 from './pages/mid1_86/Mid2NodeBlog_86';
+
+const queryClient = new QueryClient({
+  defaultoptions: {
+    queries: {
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<HomeLayout_86/>,
+    children:[
+      {
+        index: true,
+        element:<HomeLayout_86/>,
+      },
+      {
+        path: 'mid1Blog_86',
+        element:<Mid1SupaBlog_86/>,
+      },
+      {
+        path: 'mid2Blog_86',
+        element:<Mid2NodeBlog_86/>,
+      },
+      {
+        path: 'denoGetBlog_86',
+        element:<SupaGetBlog_86/>,
+      },
+    ]
+  }
+]);
+
+const App_86 = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider Router={router} />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+
+    /* <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<HomePage_86 />} />
+        <Route path='/mid2Blog_86' element={<Mid2NodeBlog_86 />} />
+        <Route path='/mid1Blog_86' element={<Mid1SupaBlog_86 />} />
+        <Route path='/demoGetBlog_86' element={<SupaGetBlog_86 />} />
+      </Routes>
+    </BrowserRouter> */
+  );
+};
+
+export default App_86;
---
+++ mid21_86/client_86/src/pages/HomeLayout_86.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+const HomeLayout_86 = () => {
+  return (
+    <>
+    <h1>
+      homeLayout_86
+    </h1>
+    <Outlet/>
+    </>
+  )
+}
+
+export default HomeLayout_86
